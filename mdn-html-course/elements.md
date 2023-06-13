af11085b6d7a13646702ba9e087a1e42684ddd78
Alexander Oakley <2579439+oakla@users.noreply.github.com>
2023-06-13 20:07:33 +1000

    first commit

---
+++ mdn-html-course/elements.md
@@ -0,0 +1,5 @@
+
+## Head
+- `meta` - charset="utf-8"
+
+- `span` - to wrap/isolate/identify content, without giving it extra meaning, so that it can be referenced and manipulated by css or js.
---
+++ trials/free-text-cells/trials.js
@@ -0,0 +1,13 @@
+
+    // Dealing with Textarea Height
+    function calcHeight(value) {
+        let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+        // min-height + lines x line-height + padding + border
+        let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+        return newHeight;
+    }
+
+    let textarea = document.querySelector(".resize-ta");
+    textarea.addEventListener("keyup", () => {
+        textarea.style.height = calcHeight(textarea.value) + "px";
+    });
